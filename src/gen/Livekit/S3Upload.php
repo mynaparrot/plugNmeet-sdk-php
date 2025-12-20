79e615792087a9079014e33e3d1c8caa72e4396b
Jibon L. Costa <jiboncosta57@gmail.com>
2025-12-20 07:58:30 +0600

    fix: bump protocol

--- src/gen/Livekit/S3Upload.php
+++ src/gen/Livekit/S3Upload.php
@@ -35,7 +35,7 @@ class S3Upload extends \Google\Protobuf\Internal\Message
     /**
      * ExternalID to use when assuming role for upload
      *
-     * Generated from protobuf field <code>string assume_role_external_id = 13;</code>
+     * Generated from protobuf field <code>string assume_role_external_id = 13 [(.logger.redact) = true];</code>
      */
     protected $assume_role_external_id = '';
     /**
@@ -197,7 +197,7 @@ class S3Upload extends \Google\Protobuf\Internal\Message
     /**
      * ExternalID to use when assuming role for upload
      *
-     * Generated from protobuf field <code>string assume_role_external_id = 13;</code>
+     * Generated from protobuf field <code>string assume_role_external_id = 13 [(.logger.redact) = true];</code>
      * @return string
      */
     public function getAssumeRoleExternalId()
@@ -208,7 +208,7 @@ class S3Upload extends \Google\Protobuf\Internal\Message
     /**
      * ExternalID to use when assuming role for upload
      *
-     * Generated from protobuf field <code>string assume_role_external_id = 13;</code>
+     * Generated from protobuf field <code>string assume_role_external_id = 13 [(.logger.redact) = true];</code>
      * @param string $var
      * @return $this
      */
